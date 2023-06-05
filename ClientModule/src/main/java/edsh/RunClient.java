4f2239320f3fe77480a4662f3e9a048222cd6280
BuildTools <unconfigured@null.spigotmc.org>
2023-06-05 17:44:04 +0300

    Last fix

--- ClientModule/src/main/java/edsh/RunClient.java
+++ ClientModule/src/main/java/edsh/RunClient.java
@@ -10,6 +10,7 @@ import java.util.HashMap;
 public class RunClient
 {
     public static void main( String[] args ) {
+
         Printer printer = new ConsolePrinter();
         HashMap<Character, String> arguments = new ArgsHelper(args).parseKeyArguments();
         String address = arguments.get('a');
